ce30970d254ec26c24d5f0099e1be0a8b61c0a88
Thibaut Lienart <ltib@me.com>
2020-06-04 00:19:44 +0200

    initial demo

--- sandbox/config.md
+++ sandbox/config.md
@@ -1,2 +1,6 @@
-@def authors = "The Oracle"
+<!-- IMPORTANT FILL THE FOLLOWING ONES -->
+@def authors = "Grace Hopper, Rick Sanchez"
 @def title = "YourPackage.jl"
+
+<!-- DO NOT CHANGE THE FOLLOWING ONES -->
+@def section_counter = 1
--- sandbox/utils.jl
+++ sandbox/utils.jl
@@ -1,16 +1,32 @@
-function hfun_bar(vname)
-  val = Meta.parse(vname[1])
-  return round(sqrt(val), digits=2)
+html(s) = "~~~$s~~~"
+
+function lx_section(com, _)
+    counter = globvar("section_counter")
+    Franklin.set_var!(Franklin.GLOBAL_VARS, "section_counter", counter+1)
+    brace_content = Franklin.content(com.braces[1])
+    name, fpath = strip.(split(brace_content, ':'))
+    isfile(fpath) || return """
+        \\style{color:red;font-weight:bold;}{File $fpath doesn't exist.}"""
+    input = read(fpath, String)
+    class = ""
+    if iseven(counter)
+        class = "class=\"bg-light\""
+    end
+    top =  html("""
+                <section id=\"$name\" $class>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-lg-8 mx-auto">
+                \n""")
+    bottom = html("""\n
+                      </div>
+                    </div>
+                  </div>
+                </section>""")
+    return top * input * bottom
 end
 
-function hfun_m1fill(vname)
-  var = vname[1]
-  return pagevar("index", var)
-end
-
-function lx_baz(com, _)
-  # keep this first line
-  brace_content = Franklin.content(com.braces[1]) # input string
-  # do whatever you want here
-  return uppercase(brace_content)
+function lx_lead(com, _)
+    txt = Franklin.content(com.braces[1])
+    return html("<p class=\"lead\">") * txt * html("</p>")
 end
