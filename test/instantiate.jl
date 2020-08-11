b59dac3d9d430d04e302e770e0b8e3611ee73a77
zdroid <zlatanvasovic@gmail.com>
2020-08-11 16:17:42 +0200

    Add more instantiate.jl tests

--- test/instantiate.jl
+++ test/instantiate.jl
@@ -2,6 +2,7 @@
     sandbox = mktempdir(); bk = pwd(); cd(sandbox)
 
     @testset "newpage" begin
+        mkdir("foo")
         newpage(path="foo", overwrite=true)
         @test isdir("foo")
         @test isdir(joinpath("foo", "_layout"))
@@ -29,10 +30,11 @@
     end
 
     @testset "optimize" begin
-        PkgPage.optimize(; input="foo", output="bar", purge=false,
+        PkgPage.optimize(; input="foo", output="bar", purge=true,
                            prerender=false)
         @test readdir(joinpath("foo", "__site")) == ["bar"]
         @test isfile(joinpath("foo", "__site", "bar", "index.html"))
+        @test isfile(joinpath("foo", "__site", "bar", "css", "bootstrap.min.css"))
     end
     cd(bk)
     Pkg.activate()
