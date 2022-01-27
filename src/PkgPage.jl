51b30d77dad7e169617b573e4dad8a649184849e
Thibaut Lienart <tlienart@me.com>
2022-01-27 21:41:33 +0100

    cleaning up

--- src/PkgPage.jl
+++ src/PkgPage.jl
@@ -11,7 +11,6 @@ export serve
 
 export lx_lead, lx_figure, lx_alert
 export env_column, env_table, env_section
-# export lx_begin, lx_end, lx_column
 
 export hfun_col
 
@@ -20,7 +19,6 @@ const F = Franklin
 include("instantiate.jl")
 
 include("latex/basic.jl")
-# include("coms/begin-end.jl")
 include("latex/environments.jl")
 
 end # module
